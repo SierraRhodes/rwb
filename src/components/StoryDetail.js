a8c8185905bb5376be26de188a5010921ee4d883
SierraRhodes <alreadywrote@gmail.com>
2023-09-14 12:55:17 -0700

    add edit/delete functionality to stories

--- src/components/StoryDetail.js
+++ src/components/StoryDetail.js
@@ -3,36 +3,33 @@
 
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
-import { doc, getDoc, collection, getDocs } from 'firebase/firestore'; // Import getDocs
+import { doc, getDoc, collection, getDocs, updateDoc, deleteDoc } from 'firebase/firestore';
 import { db } from '../firebase';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 function StoryDetails() {
   const { id } = useParams();
   const [story, setStory] = useState(null);
-  const [chapters, setChapters] = useState([]); // State to store chapters
+  const [chapters, setChapters] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedStory, setEditedStory] = useState({});
+  const navigate = useNavigate();
 
   useEffect(() => {
     const fetchStory = async () => {
       try {
-        // Reference the specific story document using the 'id' parameter
         const storyDocRef = doc(db, 'stories', id);
-
-        // Fetch the story document
         const storyDoc = await getDoc(storyDocRef);
 
         if (storyDoc.exists()) {
-          // Extract the story data from the document and set it in the state
           const storyData = storyDoc.data();
           setStory({
             id: storyDoc.id,
             title: storyData.title,
             summary: storyData.summary,
             genre: storyData.genre,
-            // Add other story properties as needed
           });
 
-          // Fetch chapters for the story
           const chaptersRef = collection(storyDocRef, 'chapters');
           const chapterQuerySnapshot = await getDocs(chaptersRef);
           const chapterData = [];
@@ -44,7 +41,6 @@ function StoryDetails() {
           });
           setChapters(chapterData);
         } else {
-          // Handle the case where the story with the provided ID doesn't exist
           console.error('Story not found.');
         }
       } catch (error) {
@@ -55,43 +51,123 @@ function StoryDetails() {
     fetchStory();
   }, [id]);
 
-  // Add a conditional check to ensure 'story' is not null before accessing its properties
+  const handleEditStoryClick = () => {
+    setIsEditing(true);
+    // Initialize editedStory with the current story data
+    setEditedStory({
+      title: story.title,
+      summary: story.summary,
+      genre: story.genre,
+    });
+  };
+
+  const handleSaveStoryClick = async () => {
+    try {
+      const storyDocRef = doc(db, 'stories', id);
+      await updateDoc(storyDocRef, editedStory);
+      setIsEditing(false);
+
+      // Reload the story data after saving (optional)
+      const reloadedStoryDoc = await getDoc(storyDocRef);
+      const reloadedStoryData = reloadedStoryDoc.data();
+      setStory({
+        id: reloadedStoryDoc.id,
+        title: reloadedStoryData.title,
+        summary: reloadedStoryData.summary,
+        genre: reloadedStoryData.genre,
+      });
+    } catch (error) {
+      console.error('Error updating story:', error);
+    }
+  };
+
+  const handleCancelStoryClick = () => {
+    setIsEditing(false);
+    // Reset editedStory to the current story data
+    setEditedStory({
+      title: story.title,
+      summary: story.summary,
+      genre: story.genre,
+    });
+  };
+
+  const handleDeleteStoryClick = async () => {
+    const confirmDelete = window.confirm('Are you sure you want to delete this story?');
+
+    if (confirmDelete) {
+      try {
+        const storyDocRef = doc(db, 'stories', id);
+        await deleteDoc(storyDocRef);
+        // Redirect or perform any other actions after deleting
+        navigate('/story-list');
+      } catch (error) {
+        console.error('Error deleting story:', error);
+      }
+    }
+  };
+
   if (!story) {
-    return <div>Loading...</div>; // You can add a loading indicator if needed.
+    return <div>Loading...</div>;
   }
 
   return (
     <div>
-      <h2>{story.title}</h2>
-      <h3>Summary</h3>
-      {story.summary && <p>{story.summary}</p>}
-      {story.genre && <p>{story.genre}</p>}
-
-      <h3>Chapters:</h3>
-      {chapters.length > 0 ? ( // Use chapters state to check if chapters exist
-        <ul>
-          {chapters.map((chapter) => (
-            <li key={chapter.id}>
-               <h4>
-                 <Link to={`/chapter-detail/${story.id}/${chapter.id}`}>{chapter.title}</Link>
-               </h4>
-              {/* {chapter.content && <p>{chapter.content}</p>} */}
-            </li>
-          ))}
-        </ul>
+      {isEditing ? (
+        <div>
+          <input
+            type="text"
+            value={editedStory.title}
+            onChange={(e) => setEditedStory({ ...editedStory, title: e.target.value })}
+          />
+          <input
+            type="text"
+            value={editedStory.summary}
+            onChange={(e) => setEditedStory({ ...editedStory, summary: e.target.value })}
+          />
+          <input
+            type="text"
+            value={editedStory.genre}
+            onChange={(e) => setEditedStory({ ...editedStory, genre: e.target.value })}
+          />
+          <button onClick={handleSaveStoryClick}>Save</button>
+          <button onClick={handleDeleteStoryClick}>Delete</button>
+          <button onClick={handleCancelStoryClick}>Cancel</button>
+        </div>
       ) : (
-        <p>No chapters available for this story.</p>
-      )}
+        <div>
+          <h2>{story.title}</h2>
+          <h3>Summary</h3>
+          {story.summary && <p>{story.summary}</p>}
+          {story.genre && <p>{story.genre}</p>}
 
-      <h3>
-        <Link to="/story-list">Back to Story List</Link>
-      </h3>
-      <h3>
-        <Link to={`/chapter?storyId=${story.id}`}>Add Chapter</Link>
-      </h3>
+          <h3>Chapters:</h3>
+          {chapters.length > 0 ? (
+            <ul>
+              {chapters.map((chapter) => (
+                <li key={chapter.id}>
+                  <h4>
+                    <Link to={`/chapter-detail/${story.id}/${chapter.id}`}>{chapter.title}</Link>
+                  </h4>
+                </li>
+              ))}
+            </ul>
+          ) : (
+            <p>No chapters available for this story.</p>
+          )}
+
+          <h3>
+            <Link to="/story-list">Back to Story List</Link>
+          </h3>
+          <h3>
+            <button onClick={handleEditStoryClick}>Edit Story</button>
+            <button onClick={handleDeleteStoryClick}>Delete Story</button>
+          </h3>
+        </div>
+      )}
     </div>
   );
 }
 
 export default StoryDetails;
+
 
