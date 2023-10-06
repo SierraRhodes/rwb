96ebe3b3005cc842a07fa4c09ae25fdd870b555f
SierraRhodes <alreadywrote@gmail.com>
2023-10-06 01:01:18 -0700

    add stylization to search button

--- src/components/Search.js
+++ src/components/Search.js
@@ -26,7 +26,23 @@ const SearchInput = styled.input`
   color: #ffffff;
   outline: none;
   margin-left: 10px;
-  margin-right: 320px;
+  margin-right: 332px;
+`;
+
+const Button = styled.button`
+background: linear-gradient(
+  45deg, 
+  rgba(153, 204, 255, 0.9), /* Adjust opacity (0.9) to make it paler */
+  rgba(102, 255, 204, 0.9)
+);
+  border-radius: 40%;
+  width: 30px;
+  height: 30px;
+  border: none;
+  position: absolute;
+  //top: 1%; /* Adjust the top and left values as needed */
+  bottom: 22px;
+  
 `;
 
 
@@ -49,6 +65,8 @@ function Search({ onSearch }) {
 
   {/* Magnifying glass icon using inline SVG */}
   <div>
+    <Button>
+
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="16"
@@ -62,6 +80,7 @@ function Search({ onSearch }) {
       d="M11.25 1a.75.75 0 0 1 .75.75V3h2.5a.5.5 0 0 1 0 1H12v3.75a2.25 2.25 0 0 1-4.5 0V4H.75a.75.75 0 0 1-.75-.75V1a.75.75 0 0 1 .75-.75h13.5zM2 7.25A1.25 1.25 0 0 1 3.25 6h9.5A1.25 1.25 0 0 1 14 7.25V14H2V7.25z"
     />
   </svg>
+  </Button>
 </div>
   {/* End of magnifying glass icon */}
   
